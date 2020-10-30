d63a2af15bc27c24983635c16a86987891a6ea04
Yurii Rysyn <zerocool_cs@ukr.net>
2020-10-30 17:02:12 +0200

    Updates

---
+++ precache-manifest.8e1d1df3b432896c5e6a9b82c34f18f8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53da41e295cee90762108dfc3611290e",
+    "url": "/OS-system-TT-catalog/index.html"
+  },
+  {
+    "revision": "19600d0e4e58291699ab",
+    "url": "/OS-system-TT-catalog/static/css/main.eb07f277.chunk.css"
+  },
+  {
+    "revision": "de1dd9a38add480d45a6",
+    "url": "/OS-system-TT-catalog/static/js/2.e2979081.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/OS-system-TT-catalog/static/js/2.e2979081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19600d0e4e58291699ab",
+    "url": "/OS-system-TT-catalog/static/js/main.f73eded4.chunk.js"
+  },
+  {
+    "revision": "f1f921b2f89b0c1b9d89",
+    "url": "/OS-system-TT-catalog/static/js/runtime-main.93aaf9e9.js"
+  },
+  {
+    "revision": "2c9eb36260b3cd2867647be779ca6477",
+    "url": "/OS-system-TT-catalog/static/media/Grandstander-Medium.2c9eb362.ttf"
+  },
+  {
+    "revision": "0a64740c61b8aa13ea5e2dd0eb4726e1",
+    "url": "/OS-system-TT-catalog/static/media/SharpSansNo2Bold.0a64740c.otf"
+  },
+  {
+    "revision": "20b773328c98c9f9c7ce59c551f05516",
+    "url": "/OS-system-TT-catalog/static/media/SharpSansNo2Medium.20b77332.otf"
+  },
+  {
+    "revision": "c90f75b930746d6bc66e82baf982ca34",
+    "url": "/OS-system-TT-catalog/static/media/SharpSansNo2Semibold.c90f75b9.otf"
+  },
+  {
+    "revision": "574923b9bca2c9c624830fefc467c4bc",
+    "url": "/OS-system-TT-catalog/static/media/bg-img.574923b9.jpg"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "/OS-system-TT-catalog/static/media/btn-left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "67be4a4a4d709745ad41a3578d953895",
+    "url": "/OS-system-TT-catalog/static/media/btn-right.67be4a4a.svg"
+  },
+  {
+    "revision": "3423a46d85e1f5075cce7cbcd0c1491a",
+    "url": "/OS-system-TT-catalog/static/media/close_menu.3423a46d.png"
+  },
+  {
+    "revision": "541f6d0ae89cfde7d3cff48dba8f4d12",
+    "url": "/OS-system-TT-catalog/static/media/menu.541f6d0a.svg"
+  }
+]);
